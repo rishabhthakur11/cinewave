a24686cbf5df8d12443b7aaddfe0af056f40104b
Rishabh Thakur <rishabhthakur046@gmail.com>
2024-02-21 22:39:16 +0530

    Made user entity and update application.properties with db credentials

--- src/main/java/com/cinewave/model/UserEntity.java
+++ src/main/java/com/cinewave/model/UserEntity.java
@@ -1,13 +1,45 @@
 package com.cinewave.model;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class UserEntity {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
+
+  @Column(name = "name", nullable = false)
   private String name;
+
+  @Column(name = "email", nullable = false)
   private String email;
+
+  @Column(name = "password", nullable = false)
   private String password;
+
+  @Column(name = "mobile", nullable = false)
   private String mobile;
+
+  @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
   private List<TicketEntity> ticketEntities;
 }
