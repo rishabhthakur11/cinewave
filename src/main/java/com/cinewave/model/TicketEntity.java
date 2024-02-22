b892929edd176d2e50ad40150401031bd215841d
Rishabh Thakur <rishabhthakur046@gmail.com>
2024-02-22 10:56:59 +0530

    TicketEntity: Add ticket Entity

--- src/main/java/com/cinewave/model/TicketEntity.java
+++ src/main/java/com/cinewave/model/TicketEntity.java
@@ -1,15 +1,59 @@
 package com.cinewave.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import java.util.Date;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
 
+@Getter
+@Setter
+@Entity
+@Table(name = "ticket")
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class TicketEntity {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
+
+  @Column(name = "alloted_seats", nullable = false)
   private String allottedSeats;
+
+  @Column(name = "amount", nullable = false)
   private double amount;
+
+  @CreatedDate
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(name = "booked_at", nullable = false)
   private Date bookedAt;
+
+  @ManyToOne
+  @JsonIgnore
   private UserEntity user;
+
+  @ManyToOne
+  @JsonIgnore
   private ShowEntity show;
+
+  @OneToMany(mappedBy = "show", cascade = CascadeType.ALL)
+  @JsonIgnore
   private List<ShowSeatsEntity> seats;
 }
